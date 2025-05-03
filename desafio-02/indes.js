9d2670c0d47b5b7c2ef866c9f7063c2fff2a1c73
Wellingtonalves33 <wellingtonvdtc@gmail.com>
2025-05-03 18:15:14 -0300

    treino

---
+++ desafio-02/indes.js
@@ -0,0 +1,9 @@
+let nota = 8
+
+if (nota < 3) {
+    console.log(`Sua nota foi ${nota}, muito horrivel`)
+} else if (nota == 3 || nota <= 7) {
+    console.log(`Sua nota foi, ${nota}. Da pra melhorar `)
+} else {
+    console.log('Nota exelente')
+}
